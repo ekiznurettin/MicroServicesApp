bc90d41f6c8fc323cb01a6a4d4ec82f0ad77d9e9
ekiznurettin <79282298+ekiznurettin@users.noreply.github.com>
2022-10-01 10:37:01 +0300

    Photo Stock.API microservisi yazıldı

--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
@@ -1,36 +1,22 @@
-using FreeCourse.Services.Catalog.Services;
-using FreeCourse.Services.Catalog.Settings;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
     options.Authority = builder.Configuration["IdentityServerURL"];
-    options.Audience = "resource_catalog";
+    options.Audience = "photo_stock_catalog";
     options.RequireHttpsMetadata = false;
 });
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddScoped<ICourseService, CourseService>();
-
 // Add services to the container.
-builder.Services.AddAutoMapper(typeof(Program));
-builder.Services.AddControllers(options => {
+
+builder.Services.AddControllers(options =>
+{
     options.Filters.Add(new AuthorizeFilter());
 });
-
-builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
-builder.Services.AddSingleton<IDatabaseSettings>(sp =>
-{
-    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
-});
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
 
 var app = builder.Build();
 
@@ -40,7 +26,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseStaticFiles();
+
 app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
